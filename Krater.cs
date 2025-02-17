5b19e4d52458d7b46f08a910ee77e13e2f6a4382
gergelyviktor <gergelyv@kkszki.hu>
2025-02-17 10:16:26 +0100

    Add project files.

---
+++ Krater.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp5 {
+    internal class Krater {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double R { get; set; }
+        public string Nev { get; set; }
+        public Krater(double x, double y, double r, string nev) { 
+            this.X = x;
+            this.Y = y;
+            this.R = r;
+            this.Nev = nev;
+        }
+    }
+}
